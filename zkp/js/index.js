8cb1d8c89a637d4d8b8f4eae4d0db209dde21426
Goutham <dkgoutham1806@gmail.com>
2026-03-31 14:30:49 +0530

    chore: default circuit artifacts to zkp/artifacts/, remove mandatory env vars

--- zkp/js/index.js
+++ zkp/js/index.js
@@ -35,10 +35,8 @@ function loadCircuit(type) {
   if (!type) {
     throw new Error('The circuit name must be provided');
   }
-  const circuitsRoot = process.env.CIRCUITS_ROOT;
-  if (!circuitsRoot) {
-    throw new Error('CIRCUITS_ROOT is not set');
-  }
+  const circuitsRoot = process.env.CIRCUITS_ROOT || path.resolve(__dirname, '..', 'artifacts');
+
   const WitnessCalculator = require(path.join(circuitsRoot, `${type}_js/witness_calculator.js`));
   const buffer = readFileSync(path.join(circuitsRoot, `${type}_js/${type}.wasm`));
   return WitnessCalculator(buffer);
